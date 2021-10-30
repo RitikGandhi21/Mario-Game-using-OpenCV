b191fce8a6870beaa560fb3c29f4e514680393aa
RITIK <ritikgandhi21@gmail.com>
2021-10-30 18:34:14 +0530

    FIRST commit

--- controls.py
+++ controls.py
@@ -10,20 +10,14 @@ global space_pressed
 key = None
 space_pressed = False
 
-# for pressing the space
-# keyboard.press(Key.space) and keyboard.release(Key.space) 
 
-# for pressing shift plus some key
-# with keyboard.pressed(Key.shift):
-# 		keyboard.press('a')
-# 		keyboard.release('a')
 
 
 def do_nothing():
     global key
     global space_pressed
 
-    # if space is pressed release it
+    
     if space_pressed:
         keyboard.release(Key.space)
         space_pressed = False
@@ -36,7 +30,7 @@ def move_left():
     global key
     global space_pressed
 
-    # if space is pressed release it
+    
     if space_pressed:
         keyboard.release(Key.space)
         space_pressed = False
@@ -52,7 +46,7 @@ def move_right():
     global key
     global space_pressed
     
-    # if space is pressed release it
+    
     if space_pressed:
         keyboard.release(Key.space)
         space_pressed = False
@@ -64,19 +58,19 @@ def move_right():
     for i in range(100):
         keyboard.press(key)
 
-    #print("key pressed")
+    
 
 def jump():
 
     global key
     global space_pressed 
 
-    # if some other key is pressed release it
+    
     if key != None:
         keyboard.release(key)
 
     keyboard.press(Key.space)
 
     space_pressed = True
-    #print("Space pressed")
+   
 
--- main.py
+++ main.py
@@ -48,18 +48,16 @@ with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence= 0.5) as
                 t1 = threading.Thread(target=move_left)
                 t1.start()
 
-            # # if x co-ordinate passes right threshold move right
             if int(mid_point_of_shoulder[0]) > int(right_threshold):
-                # for i in range(400):
-                #     move_right() 
+                
                 t2 = threading.Thread(target=move_right)
                 t2.start()               
                 
 
-            # # if y co-ordinate passes aboe the top threshold move up
+            
             if int(mid_point_of_shoulder[1]) < int(top_threshold):
                 jump()
-                #print("jump")
+                
 
             else:
                 do_nothing()
@@ -70,12 +68,7 @@ with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence= 0.5) as
             print("enter exception", e)
             do_nothing()
 
-        # rendering the detections
-        # to show the acutal lines on the body
-        # mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_pose.POSE_CONNECTIONS,
-        #                                   mp_drawing.DrawingSpec(color=(245, 117, 66), thickness=2, circle_radius=2),
-        #                                   mp_drawing.DrawingSpec(color=(245, 66, 230), thickness=2, circle_radius=2)
-        #                                   )
+        
         cv2.imshow('Cam Feed', image)
 
         if cv2.waitKey(10) & 0xFF == ord('q'):
